9b819ab4e1b5da389fbb7c47742a2770c6b88aed
Marie Vartoryan <vartoryan@bk.ru>
2022-03-20 12:49:30 +0100

    Add functionality for adding links.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, abort, redirect
+import hashlib
+
+from flask import Flask, abort, redirect, request
 
 app = Flask(__name__)
 
@@ -12,6 +14,19 @@ def redirect_to_saved_link(hash):
         return redirect(FAKE_TABLE[hash], code=301)
     else:
         return abort(404)
+
+
+@app.route('/add', methods=['POST'])
+def add_short_link():
+    try:
+        url = request.json['url']
+    except KeyError:
+        return '"url" was expected in body', 400
+
+    hash_url = hashlib.md5(url.encode('UTF-8')).hexdigest()
+    FAKE_TABLE[hash_url] = url
+    return f'{request.url_root}{hash_url}'
+
 
 if __name__ == '__main__':
     app.run()
